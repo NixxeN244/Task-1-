46ceaaa051c34a2aaa82ae548b7f60d0ffd0d287
NixxeN244 <ns233282@gmail.com>
2020-09-12 16:06:40 +0200

    completion of Q2.4 for the enemy class

--- TASK 1-Code/Enemy.cs
+++ TASK 1-Code/Enemy.cs
@@ -9,4 +9,25 @@ namespace TASK_1_Code
     class Enemy : Character
     {
         //commit changes
+        protected Random randomobj;
+
+        public Random Randomobj
+        {
+            get { return randomobj; }
+            set { randomobj = value; }
+        }
+
+        public Enemy(int Xvalue, int Yvalue, int max_HP, int Enemy_dmage, char symbol) : base(Xvalue, Yvalue, symbol)
+        {
+            Xvalue = this.Xvalue;
+            Yvalue = this.Yvalue;
+            max_HP = this.Max_HP;
+            Enemy_dmage = this.Damage;
+        }
+
+        public override string ToString()
+        {
+          return string.Format(this.GetType().ToString() + this.Xvalue + this.Yvalue + this.Damage); 
+        }
+    }
 }
