886ba628755b9456e5ecc223efd17fc5bd7a6df0
NixxeN244 <ns233282@gmail.com>
2020-09-12 18:05:43 +0200

    Properties of the map class created

--- TASK 1-Code/Map.cs
+++ TASK 1-Code/Map.cs
@@ -8,5 +8,41 @@ namespace TASK_1_Code
 {
     class Map
     {
+        private char[,] gameMap;    //the char array that will be displayed. IE our map UI.
+        private Hero playerobj;
+        private Enemy[] enemyArray;
+        private int mapwidth;
+        private int mpaheight;
+        private Random randomNum;
+
+
+        public char[,] GameMap
+        {
+            get { return gameMap; }
+            set { gameMap = value; }
+        }
+
+        public Hero PlayerObj
+        {
+            get { return playerobj; }
+            set { playerobj = value; }
+        }
+
+        public Enemy[] EnemeyArray
+        {
+            get { return enemyArray; }
+            set { enemyArray = value; }
+        }
+
+        public int MapWidth
+        {
+            get { return mapwidth; }
+            set { mapwidth = value; }
+        }
+        public int MapHeight
+        {
+            get { return mpaheight; }
+            set { mpaheight = value; }
+        }
     }
 }
