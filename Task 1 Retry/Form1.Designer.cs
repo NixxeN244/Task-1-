666880b4e5c0a62f5113ac33fd3175e7d374d31a
NixxeN244 <ns233282@gmail.com>
2020-09-13 20:42:16 +0200

    Setting up the UI for the game

--- Task 1 Retry/Form1.Designer.cs
+++ Task 1 Retry/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.GameMapUILbl = new System.Windows.Forms.Label();
+            this.PlayerStatsLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GameMapUILbl
+            // 
+            this.GameMapUILbl.AutoSize = true;
+            this.GameMapUILbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.GameMapUILbl.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameMapUILbl.Location = new System.Drawing.Point(226, 145);
+            this.GameMapUILbl.Name = "GameMapUILbl";
+            this.GameMapUILbl.Size = new System.Drawing.Size(64, 19);
+            this.GameMapUILbl.TabIndex = 0;
+            this.GameMapUILbl.Text = "label1";
+            // 
+            // PlayerStatsLbl
+            // 
+            this.PlayerStatsLbl.AutoSize = true;
+            this.PlayerStatsLbl.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayerStatsLbl.Location = new System.Drawing.Point(569, 25);
+            this.PlayerStatsLbl.Name = "PlayerStatsLbl";
+            this.PlayerStatsLbl.Size = new System.Drawing.Size(125, 68);
+            this.PlayerStatsLbl.TabIndex = 1;
+            this.PlayerStatsLbl.Text = "Player Stats:\r\nHp:\r\nDamage:\r\n{X,Y]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PlayerStatsLbl);
+            this.Controls.Add(this.GameMapUILbl);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label GameMapUILbl;
+        private System.Windows.Forms.Label PlayerStatsLbl;
     }
 }
 
--- Task 1 Retry/GameEngine.cs
+++ Task 1 Retry/GameEngine.cs
@@ -13,12 +13,18 @@ namespace Task_1_Retry
         private static readonly char Goblin = 'G';
         private static readonly char Empty = ' ';
         private static readonly char Obstacle = 'X';
+        private string parseMap;
 
 
         public GameEngine()
         {
             Map gameMap = new Map(1, 11, 1, 11, 3);
 
+        }
+        public string ParseMap
+        {
+            get { return parseMap; }
+            set { parseMap = value; }
         }
         public Map Gmap
         {
@@ -33,16 +39,16 @@ namespace Task_1_Retry
 
         public override string ToString()
         {
-            string parseMap;
+            
             for (int i = 0; i < Gmap.GameMap.GetLength(0); i++)
             {
                 for (int k = 0; k < Gmap.GameMap.GetLength(1); k++)
                 {
-                    parseMap = Gmap.GameMap[i, k].ToString();
+                    ParseMap = Gmap.GameMap[i, k].ToString();
                 }
-                parseMap += "\n";
+                
             }
-
+            return ParseMap;
         }
     }
 }
